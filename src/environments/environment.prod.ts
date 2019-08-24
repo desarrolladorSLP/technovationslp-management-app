d1c4d54dbdd860faa2abb28f566ca76faa8cf970
iuresti <iuresti@skytouchtechnology.com>
2019-08-23 23:23:17 -0500

    Fix npm dependencies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBOv1qnMLeOrEumB2KXe4MCwtlf9nM_l7c',
+    authDomain: 'crud-bbdd1.firebaseapp.com',
+    databaseURL: 'https://crud-bbdd1.firebaseio.com',
+    projectId: 'crud-bbdd1',
+    storageBucket: 'crud-bbdd1.appspot.com',
+    messagingSenderId: '437815419068',
+    appId: '1:437815419068:web:a5dd6197f860c863'
+  }
 };

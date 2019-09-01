cff672268baee44235a7bd630f6ea6c22e8655ba
iuresti <iuresti@skytouchtechnology.com>
2019-09-01 16:40:19 -0500

    #18 - Add process to create environment.ts from environment variables

---
+++ set-env.ts
@@ -0,0 +1,36 @@
+import {writeFileSync} from "fs";
+
+
+// Configure Angular `environment.ts` file path
+const targetPath = './src/environments/environment.ts';
+
+// Load node modules
+const colors = require('colors');
+require('dotenv').load();
+
+// `environment.ts` file structure
+console.log('A0');
+const envConfigFile: string = `export const environment = {
+  backendUrl: '${process.env.BACKEND_URL}',
+  client: {
+    username: '${process.env.CLIENT_USERNAME}',
+    password: '${process.env.CLIENT_PASSWORD}'
+  },
+  firebaseConfig: {
+    apiKey: '${process.env.FIREBASE_API_KEY}',
+    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+    databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}'
+  },
+  production: ${process.env.PRODUCTION}
+};
+`;
+
+console.log(colors.magenta(`The file 'environment.ts' will be written to ${targetPath} with the following content: \n`));
+console.log(colors.grey(envConfigFile));
+writeFileSync(targetPath, envConfigFile, {
+  encoding: 'utf-8'
+});

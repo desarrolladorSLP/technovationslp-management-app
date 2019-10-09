2122fc3c8af4ce91efed26109f456fd10712a66b
AntonioVarela <sanguinetiara31@gmail.com>
2019-10-09 16:18:54 -0500

    Creation of deliverables component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
-import {AngularFireAuth} from '@angular/fire/auth';
-import {HttpClient, HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ProgramsComponent } from './components/programs/programs.component';
 
 
-import {environment} from '../environments/environment';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {Page404Component} from './components/page404/page404.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {RouterModule, Routes} from '@angular/router';
+import { environment } from '../environments/environment';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Page404Component } from './components/page404/page404.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
 import { TokenInterceptor } from './services/auth/token.interceptor';
 import { ProgramCardComponent } from './components/program-card/program-card.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FilterPipe } from './pipes/filter.pipe';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { DeliverablesComponent } from './components/deliverables/deliverables.component';
 
 const ROUTES: Routes = [
-  {path: '', redirectTo: '/', pathMatch: 'full'},
-  {path: 'programs', component: ProgramsComponent},
-  {path: '**', component: Page404Component}
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'programs', component: ProgramsComponent },
+  { path: 'deliverables', component: DeliverablesComponent },
+  { path: '**', component: Page404Component }
 ];
 
 @NgModule({
@@ -36,6 +38,7 @@ const ROUTES: Routes = [
     ProgramsComponent,
     ProgramCardComponent,
     FilterPipe,
+    DeliverablesComponent,
   ],
   imports: [
     BrowserModule,
@@ -59,7 +62,7 @@ const ROUTES: Routes = [
     useClass: TokenInterceptor,
     multi: true
   }
-],
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {

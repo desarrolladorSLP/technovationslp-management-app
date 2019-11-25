28d03cf8200f751cad3ef4d5fea007e8487a388f
MarycruzBustos <bhmary25@gmail.com>
2019-11-25 14:58:52 -0600

    show sessions in the calendar

--- src/app/components/sessions/sessions.component.ts
+++ src/app/components/sessions/sessions.component.ts
@@ -21,9 +21,7 @@ export class SessionsComponent {
   programId: string;
   addSession = false;
   searchSession = false;
-  currentDate = new Date(2020, 4, 25);
-
-
+  currentDate = new Date(2020, 1, 27);
 
   constructor(private programService: ProgramsService, private sessionsService: SessionsService ) {
     this.programService.getPrograms().subscribe(data => {
@@ -35,6 +33,7 @@ export class SessionsComponent {
       this.sessionsService.getBatchByPrograms(programId).subscribe(data => {
         this.listBatch = data;
         this.searchSession = true;
+        console.log(this.listBatch);
         this.session = new Session();
         this.session.batchId = this.listBatch[0].id;
         this.refreshSessions(this.listBatch[0].id);
@@ -57,68 +56,105 @@ export class SessionsComponent {
   refreshSessions(batchId: string) {
     this.sessionsService.getSessionsByBatch(batchId).subscribe( data => {
         this.listSessions = data;
+        this.listSessions.forEach(element => {
+          const arrayDate = element.date.split('-');
+          const arrayStartTime = element.startTime.split(':');
+          const arrayEndTime = element.endTime.split(':');
 
+          /* console.log(Number(arrayDate[0]));
+          console.log(Number(arrayDate[1]));
+          console.log(Number(arrayDate[2]));
+          console.log(Number(arrayStartTime[0]));
+          console.log(Number(arrayStartTime[1]));
+          console.log(Number(arrayEndTime[0]));
+          console.log(Number(arrayEndTime[1])); */
+          const year = Number(arrayDate[0]);
+          const month = Number(arrayDate[1]) - 1;
+          const day = Number(arrayDate[2]);
+
+          const startHour = Number(arrayStartTime[0]);
+          const startMinutes = Number(arrayStartTime[1]);
+          const endHour = Number(arrayEndTime[0]);
+          const endMinutes = Number(arrayEndTime[1]);
+
+          /* console.log(year + '/' + month + '/' + day);
+          console.log(new Date(year, month, day , startHour, startMinutes));
+          console.log(new Date(year, month, day , endHour, endMinutes)); */
+
+          element.startDate = new Date(year, month, day , startHour, startMinutes);
+          element.endDate = new Date(year, month, day , endHour, endMinutes);
+          });
+          console.log(this.listSessions);
         this.searchSession = true;
     });
   }
 
   onAppointmentFormOpening(data) {
     const that = this;
+    console.log(data);
     const form = data.form;
-    var dateS = data.appointmentData.Date;
 
-    form.option("items", [{
-        label: {
-            text: "Title"
-        },
-        editorType: "dxTextBox",},
+    let startDate = data.appointmentData.startDate;
 
+    form.option('items', [{
+        label: {
+            text: 'Title'
+        },
+        editorType: 'dxTextBox',
+        dataField: 'title'
+      },
+
+        {            label: {
+            text: 'Notes'
+        },
+        name: 'notes',
+        editorType: 'dxTextArea',
+        dataField: 'notes'
+        },
         {
-            label: {
-            text: "Notes"
+        lbel: {
+            text: 'startTime'
         },
-        name: "notes",
-        editorType: "dxTextArea",
-        },
-        {
-        label: {
-            text: "startTime"
-        },
-          name: "startTime",
-          editorType: "dxDateBox",
+          name: 'startTime',
+          editorType: 'dxDateBox',
+          dataField: 'startTime',
           editorOptions: {
-              width: "100%",
-              type: "time",
-          }
-       },
-       {
-        label: {
-            text: "endTime"
-        },
-          name: "endTime",
-          editorType: "dxDateBox",
-          editorOptions: {
-              width: "100%",
-              type: "time",
-          }
-       },
-       {
-        label: {
-            text: "Date"
-        },
-          name: "dateS",
-          editorType: "dxDateBox",
-          editorOptions: {
-              width: "100%",
-              type: "date",
+              width: '100%',
+              type: 'time',
               onValueChanged: function(args) {
-                dateS = args.value;
+                console.log(args.value);
+            }
+          }
+       },
+       {
+        label: {
+            text: 'endTime'
+        },
+          name: 'endTime',
+          editorType: 'dxDateBox',
+          dataField: 'endTime',
+          editorOptions: {
+              width: '100%',
+              type: 'time',
+          }
+       },
+       {     label: {
+            text: 'Date'
+        },
+          name: 'startDate',
+          dataField: 'startDate',
+          editorType: 'dxDateBox',
+          editorOptions: {
+              width: '100%',
+              type: 'date',
+              onValueChanged: function(args) {
+                startDate = args.value;
             }
           }
        },
         {
-            dataField: "location",
-            editorType: "dxTextBox",
+            dataField: 'location',
+            editorType: 'dxTextBox',
         },
     ]);
 }
--- src/app/model/session.ts
+++ src/app/model/session.ts
@@ -1,12 +1,14 @@
+import { Time } from "@angular/common";
+
 export class Session {
     id: string;
     batchId: string;
     date: string;
     location: string;
-    startTime:  string;
+    startTime: string;
     endTime: string;
     title: string;
     notes: string;
-    startDate: Date = new Date();
-    endDate:Date = new Date();
+    startDate: Date;
+    endDate: Date;
 }
